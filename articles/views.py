98549924ce2e059ab47958623a7e90a4c3744294
swarina <swarinajaiswal@gmail.com>
2021-04-25 02:34:10 +0530

    Authorisation configured

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.mixins import LoginRequiredMixin 
+from django.core.exceptions import PermissionDenied
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
@@ -6,28 +8,47 @@ from django.urls import reverse_lazy
 from .models import Article
 
 # Create your views here.
-class ArticleListView(ListView):
+class ArticleListView(LoginRequiredMixin, ListView):
     model = Article
     template_name = 'article_list.html'
+    login_url = 'login'
 
-class ArticleDetailView(DetailView): 
+class ArticleDetailView(LoginRequiredMixin, DetailView): 
     model = Article
     template_name = 'article_detail.html'
+    login_url = 'login'
 
-class ArticleUpdateView(UpdateView):
+class ArticleUpdateView(LoginRequiredMixin, UpdateView):
     model = Article
     fields = ('title', 'body',)
     template_name = 'article_edit.html'
+    login_url = 'login'
 
-class ArticleDeleteView(DeleteView):
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if obj.author != self.request.user:
+            raise PermissionDenied
+        return super().dispatch(request, *args, **kwargs)
+
+class ArticleDeleteView(LoginRequiredMixin, DeleteView):
     model = Article
     template_name = 'article_delete.html'
     success_url = reverse_lazy('article_list')
+    login_url = 'login'
 
-class ArticleCreateView(CreateView):
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if obj.author != self.request.user:
+            raise PermissionDenied
+        return super().dispatch(request, *args, **kwargs)
+
+
+
+class ArticleCreateView(LoginRequiredMixin, CreateView):
     model = Article
     template_name = 'article_new.html'
     fields = ('title', 'body')
+    login_url = 'login'
 
     def form_valid(self, form): 
         form.instance.author = self.request.user
